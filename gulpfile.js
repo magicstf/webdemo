8bf744a793a959062e1ef368767f5b224fb79c47
tengfeisu <tengfeisu@sohu-inc.com>
2016-07-07 16:31:32 +0800

    update config

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,14 @@ var gulp = require('gulp'),//基础库
     rename= require('gulp-rename'),//重命名
     concat = require('gulp-concat'),//文件合并
     htmlmin = require('gulp-htmlmin'),//html文件压缩
-    rev = require('gulp-rev-append'),//给页面的引用添加版本号，清除页面引用缓存
     autoprefixer = require('gulp-autoprefixer'),//css自动添加前缀
     cssmin = require('gulp-minify-css'),//css压缩
     less = require('gulp-less'),//less
     notify = require('gulp-notify'),//显示报错信息和报错后不终止当前gulp任务
     plumber = require('gulp-plumber'), //出现异常并不终止watch事件
+    rev = require('gulp-rev'); // 为文件添加MD5码
+    revCollector = require('gulp-rev-collector'); // 将文件替换为MD5版本的文件
+    processhtml = require('gulp-processhtml');
     livereload = require('gulp-livereload'); //自动刷新
     //sass = require('gulp-ruby-sass'),//sass
     //产出路径
@@ -37,6 +39,7 @@ var gulp = require('gulp'),//基础库
         js: 'src/js/*.*',
         music: 'src/music/*.*'
     };
+
     //单个文件删除
     gulp.task('cleanHtml',function(){
         return gulp.src(destPath.html)
@@ -63,8 +66,9 @@ var gulp = require('gulp'),//基础库
             .pipe(clean({force:true}))
     });
     //图片处理
-    gulp.task('image',function(){
+    gulp.task('image',['cleanImage'],function(){
         return gulp.src(srcPath.images)
+            .pipe(plumber({errorHandler:notify.onError('Error:<%= error.message %>')}))
             .pipe(cache(imagemin({
                 progressive:true,//类型：Boolean 默认：false 无损压缩jpg图片
                 svgoPlugins:[{removeViewBox:false}],//不要移除svg的viewbox属性
@@ -73,27 +77,43 @@ var gulp = require('gulp'),//基础库
             .pipe(gulp.dest(destPath.images))
             .pipe(livereload())
     });
-    //脚本处理
-    gulp.task('js',function(){
+    //线上脚本处理
+    gulp.task('jsProd',['cleanJs'],function(){
         return gulp.src(srcPath.js)
+            .pipe(plumber({errorHandler:notify.onError('Error:<%= error.message %>')}))
             .pipe(jshint({'expr':true}))
             .pipe(jshint.reporter('default')) // 对代码进行报错提示
             .pipe(uglify({
-                mangle:true, //类型：Boolean 默认：true 是否修改变量名
-                compress:true //类型：Boolean 默认：true 是否完全压缩
+                mangle:false, //类型：Boolean 默认：true 是否修改变量名
+                compress:false //类型：Boolean 默认：true 是否完全压缩
             }))
             .pipe(concat('main.js')) //合并后的文件名
             .pipe(rename({
                 //prefix: 'stf-',
                 suffix: '.min'
             }))
+            .pipe(rev()) //生成MD5版本文件
             .pipe(gulp.dest(destPath.js))
+            .pipe(rev.manifest('manifest-js.json',{merge:true})) // 生成原文件与MD5版本文件的映射文件
+            .pipe(gulp.dest(destPath.html+'rev/')) //将生成的映射文件添加到rev文件夹下
             .pipe(livereload())
     });
-    //HTML处理
-    gulp.task('html',function(){
+    //开发脚本处理
+    gulp.task('jsDev',['cleanJs'],function(){
+        return gulp.src(srcPath.js)
+            .pipe(plumber({errorHandler:notify.onError('Error:<%= error.message %>')}))
+            .pipe(jshint({'expr':true}))
+            .pipe(jshint.reporter('default')) // 对代码进行报错提示
+            .pipe(rev())
+            .pipe(gulp.dest(destPath.js))
+            .pipe(rev.manifest('manifest-js.json',{merge:true}))
+            .pipe(gulp.dest(destPath.html+'rev/'))
+            .pipe(livereload())
+    });
+    //线上HTML处理
+    gulp.task('htmlProd',['cleanHtml'],function(){
         var options = {
-            removeComments: true,//清除HTML注释
+            removeComments: false,//清除HTML注释
             collapseWhitespace: false,//压缩HTML
             collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
             removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
@@ -103,13 +123,32 @@ var gulp = require('gulp'),//基础库
             minifyCSS: true//压缩页面CSS
         };
         return gulp.src(srcPath.html)
-            .pipe(rev()) //给页面的引用添加版本号，清除页面引用缓存。
+            .pipe(plumber({errorHandler:notify.onError('Error:<%= error.message %>')}))
+            .pipe(processhtml())
             .pipe(htmlmin(options))
             .pipe(gulp.dest(destPath.html))
             .pipe(livereload())
     });
-    //CSS处理
-    gulp.task('css',function(){
+    //开发HTML处理
+    gulp.task('htmlDev',['cleanHtml'],function(){
+        var options = {
+            removeComments: false,//清除HTML注释
+            collapseWhitespace: false,//压缩HTML
+            collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+            removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+            removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+            removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/less"
+            minifyJS: false,//压缩页面JS
+            minifyCSS: true//压缩页面CSS
+        };
+        return gulp.src(srcPath.html)
+            .pipe(plumber({errorHandler:notify.onError('Error:<%= error.message %>')}))
+            .pipe(htmlmin(options))
+            .pipe(gulp.dest(destPath.html))
+            .pipe(livereload())
+    });
+    //线上CSS处理
+    gulp.task('cssProd',['cleanCss'],function(){
         return gulp.src(srcPath.css)
             .pipe(plumber({errorHandler:notify.onError('Error:<%= error.message %>')}))
             .pipe(less())
@@ -123,32 +162,68 @@ var gulp = require('gulp'),//基础库
                 compatibility:'ie8', //类型：String 默认：''or'*' eg: 'ie8'：IE8兼容模式
                 keepBreaks:true //类型：Boolean 默认：false [是否保留换行]
             }))
+            .pipe(concat('main.css')) //合并后的文件名
             .pipe(rename({
                 suffix:'.min'
             }))
+            .pipe(rev())
             .pipe(gulp.dest(destPath.css))
+            .pipe(rev.manifest('manifest-css.json',{merge:true}))
+            .pipe(gulp.dest(destPath.html+'rev/'))
+            .pipe(livereload())
+    });
+    //开发CSS处理
+    gulp.task('cssDev',['cleanCss'],function(){
+        return gulp.src(srcPath.css)
+            .pipe(plumber({errorHandler:notify.onError('Error:<%= error.message %>')}))
+            .pipe(less())
+            .pipe(autoprefixer({
+                browsers:['> 5%','last 2 versions','Android >= 4.0','IOS >=6'],
+                cascade:true, //是否美化属性值 默认：true
+                remove:true //是否去掉不必要的前缀 默认：true
+            }))
+            .pipe(rev())
+            .pipe(gulp.dest(destPath.css))
+            .pipe(rev.manifest('manifest-css.json',{merge:true}))
+            .pipe(gulp.dest(destPath.html+'rev/'))
             .pipe(livereload())
     });
     //字体处理
-    gulp.task('font',function(){
+    gulp.task('font',['cleanFonts'],function(){
         return gulp.src(srcPath.fonts)
             .pipe(gulp.dest(destPath.fonts))
             .pipe(livereload())
     });
     //音频处理
-    gulp.task('music',function(){
+    gulp.task('music',['cleanMusic'],function(){
         return gulp.src(srcPath.music)
             .pipe(gulp.dest(destPath.music))
             .pipe(livereload())
     });
+    //将html中引入的js,css文件替换为MD5版本的文件(线上)
+    gulp.task('revHTMLProd',['jsProd','cssProd'],function(){
+        return gulp.src([destPath.html+'rev/*.json',destPath.html+'*.html'])
+            .pipe(revCollector()) // 将HTML中不带MD5码的文件按照rev生成的映射关系替换为MD5版本的文件
+            .pipe(gulp.dest(destPath.html))
+    })
+    //将html中引入的js,css文件替换为MD5版本的文件(开发)
+    gulp.task('revHTMLDev',['jsDev','cssDev'],function(){
+        return gulp.src([destPath.html+'rev/*.json',destPath.html+'*.html'])
+            .pipe(revCollector())
+            .pipe(gulp.dest(destPath.html))
+    })
     //文件统一清理
     gulp.task('cleanAll',function(){
         return gulp.src([destPath.html,destPath.css,destPath.fonts,destPath.images,destPath.js,destPath.music])
             .pipe(clean({force:true}))
     });
-    //任务统一编译
-    gulp.task('demo',['cleanAll'],function(){
-        gulp.start('html','js','css','image','font','music');
+    //上线任务统一编译
+    gulp.task('demoProd',['cleanAll'],function(){
+        gulp.start('htmlProd','jsProd','cssProd','image','font','music','revHTMLProd');
+    });
+    //开发任务统一编译
+    gulp.task('demoDev',['cleanAll'],function(){
+        gulp.start('htmlDev','jsDev','cssDev','image','font','music','revHTMLDev');
     });
     //变动统一监听
     gulp.task('demoWatch',function(){
